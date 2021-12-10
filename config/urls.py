921066a2e16fd62d31d18f25dc2a78772f97b4b3
Mexrojxon <mexrojxon.dev@gmail.com>
2021-12-10 17:32:05 +0500

    Start

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
+    path('category/', include('categories.urls')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
